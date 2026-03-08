dc6b938ef83389c3971b743c0485a58a930e6a70
kalebtavares <kalebtavares@ucloud.com>
2026-03-08 15:14:37 -0400

    Comit initial

---
+++ src/main/java/com/example/crud/model/Product.java
@@ -0,0 +1,45 @@
+package com.example.crud.model;
+
+import jakarta.persistence.*;
+import org.hibernate.grammars.hql.HqlParser;
+
+@Entity(name = "product")
+@Table(name = "product")
+
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private Long price;
+
+    public Product(Integer id, String name, long price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+}
