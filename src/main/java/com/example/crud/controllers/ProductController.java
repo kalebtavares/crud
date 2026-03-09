2d3701341b1c82ce1a4e16901fa13ec411a6a3a1
kalebtavares <kalebtavares@ucloud.com>
2026-03-08 21:28:42 -0400

    Bug Fix, testes via insomnia.

---
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -0,0 +1,14 @@
+package com.example.crud.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+    @GetMapping
+    public String ola(){
+        return "Olá Mundo";
+    }
+}
