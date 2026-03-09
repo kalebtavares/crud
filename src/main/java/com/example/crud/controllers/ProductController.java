5c8fd9ffceef9928fc9ef00d8c3dbdc700190fe4
kalebtavares <kalebtavares@ucloud.com>
2026-03-09 13:19:32 -0400

    feat(product): Adiciona endpoints POST para produtos

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -5,10 +5,7 @@ import com.example.crud.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +20,10 @@ public class ProductController {
     public ResponseEntity getAll(){
         List<Product> listProducts = repository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(listProducts);
+    }
+
+    @PostMapping
+    public ResponseEntity (@RequestBody Product product) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(product));
     }
 }
