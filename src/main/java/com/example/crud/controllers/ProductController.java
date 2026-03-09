70f05fb39492083bb15e5d38f3a63cdb55b009cc
kalebtavares <kalebtavares@ucloud.com>
2026-03-08 22:02:41 -0400

    Bug Fix, testes via insomnia.

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -1,14 +1,27 @@
 package com.example.crud.controllers;
 
+import com.example.crud.model.Product;
+import com.example.crud.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    ProductRepository repository;
+
     @GetMapping
-    public String ola(){
-        return "Olá Mundo";
+    public ResponseEntity getAll(){
+        List<Product> listProducts = repository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(listProducts);
     }
 }
