acaab17f713f367da48ff3e889079a0f9aca8242
ktavares <kalebtavares@icloud.com>
2026-03-09 22:50:39 -0400

    feat(product): Adiciona endpoint para atualização de produto
    Adiciona um novo endpoint PUT para atualizar produtos por ID, utilizando BeanUtils para cópia de propriedades do DTO para o modelo.

--- src/main/java/com/example/crud/controllers/ProductController.java
+++ src/main/java/com/example/crud/controllers/ProductController.java
@@ -20,7 +20,7 @@ public class ProductController {
     ProductRepository repository;
 
     @GetMapping
-    public ResponseEntity getAll(){
+    public ResponseEntity getAll() {
         List<Product> listProducts = repository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(listProducts);
     }
@@ -30,7 +30,7 @@ public class ProductController {
 
         Optional product = repository.findById(id);
 
-        if(product.isEmpty()) {
+        if (product.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not Found");
         }
         return ResponseEntity.status(HttpStatus.FOUND).body(product.get());
@@ -43,16 +43,31 @@ public class ProductController {
         BeanUtils.copyProperties(dto, product);
         return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(product));
     }
+
     @DeleteMapping("/{id}")
     public ResponseEntity delete(@PathVariable(value = "id") Integer id) {
 
-        Optional <Product> product = repository.findById(id);
+        Optional<Product> product = repository.findById(id);
 
-        if(product.isEmpty()) {
+        if (product.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not Found");
         }
         repository.delete(product.get());
         return ResponseEntity.status(HttpStatus.OK).body("Product deleted");
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity update(@PathVariable(value = "id") Integer id, @RequestBody ProductsDto dto) {
+
+        Optional<Product> product = repository.findById(id);
+
+        if (product.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not Found");
+        }
+        var productModel = product.get();
+        BeanUtils.copyProperties(dto, productModel);
+
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(productModel));
+
+    }
 }
